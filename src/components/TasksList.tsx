a3e877a11a26d339b3fdc3ee3d38559850116c06
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-01-05 16:36:37 -0300

    Challenge part 1 fixed the tasks counter

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Image, TouchableOpacity, View, Text, StyleSheet, FlatListProps } from 'react-native';
+import { FlatList, Image, TouchableOpacity, View, Text, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import { ItemWrapper } from './ItemWrapper';
